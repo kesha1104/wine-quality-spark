995262a2eb6963ec9a4d68c1bd77291dd75b407d
Kesha Dave <96973744+kesha1104@users.noreply.github.com>
2025-11-23 16:34:53 -0500

    Update predict_app.py

--- predict/predict_app.py
+++ predict/predict_app.py
@@ -1,1 +1,39 @@
+from pyspark.sql import SparkSession
+from pyspark.ml import PipelineModel
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: predict_app.py <test_csv> <model_path>")
+        sys.exit(1)
+
+    test_csv = sys.argv[1]
+    model_path = sys.argv[2]
+
+    spark = SparkSession.builder.appName("WineQualityPrediction").getOrCreate()
+
+    # Load CSV
+    test_df = spark.read.csv(test_csv, header=True, sep=';', inferSchema=True)
+
+    # Load model
+    model = PipelineModel.load(model_path)
+
+    # Predict
+    preds = model.transform(test_df)
+
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol="label",
+        predictionCol="prediction",
+        metricName="f1"
+    )
+
+    f1 = evaluator.evaluate(preds)
+    print(f"F1 Score on Test Data: {f1}")
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
 
